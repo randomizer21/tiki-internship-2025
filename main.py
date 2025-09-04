290ba1e142b78653d7360de93977f14c1d4265fe
Dannyv <danielvalledubon@gmail.com>
2025-09-04 09:45:26 -0700

    completion of initial task

---
+++ main.py
@@ -0,0 +1,11 @@
+def fullName(firstName, lastName):
+    print(firstName , lastName)
+
+firstName = ""
+lastName = ""
+
+fullName(firstName, lastName)
+
+# for testing break points
+print("test")
+print("test2")
